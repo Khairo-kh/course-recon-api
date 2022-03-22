41972f2e51054084198c194092d8454cee52fb56
Khairo <khairo.khatib@gmail.com>
2022-03-22 17:59:39 -0400

    clean up of the next template and switching to ts

---
+++ rmc-web/src/pages/index.tsx
@@ -0,0 +1,5 @@
+import { Box } from '@chakra-ui/react';
+
+const Index = () => <Box>Hello Chakra!</Box>;
+
+export default Index;
---
+++ rmc-web/src/pages/register.tsx
@@ -0,0 +1,9 @@
+import { Box } from '@chakra-ui/react';
+
+interface registerProps {}
+
+const Register: React.FC<registerProps> = ({}) => {
+    return <Box>registration page</Box>;
+};
+
+export default Register;
