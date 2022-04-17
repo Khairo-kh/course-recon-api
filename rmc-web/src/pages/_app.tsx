e5ce9bc6d9104414a9190aa21683f7be8c7875e0
Khairo <khairo.khatib@gmail.com>
2022-04-17 01:19:32 -0400

    feat: resolved urql query caching problem

--- rmc-web/src/pages/_app.tsx
+++ rmc-web/src/pages/_app.tsx
@@ -1,11 +1,67 @@
+import { queries } from '@chakra-ui/color-mode/dist/declarations/src/color-mode.utils';
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
-import { Provider, createClient } from 'urql';
+import { Cache, cacheExchange, QueryInput } from '@urql/exchange-graphcache';
+import { ResolveLayoutTransition } from 'framer-motion';
+import { Provider, createClient, dedupExchange, fetchExchange } from 'urql';
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from '../generated/graphql';
 
 import theme from '../theme';
+
+function typedUpdateQuery<Result, Query>(
+  cache: Cache,
+  input: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(input, (data) => fn(result, data as any) as any);
+}
 
 const client = createClient({
   url: 'http://localhost:8000/graphql',
   fetchOptions: { credentials: 'include' },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            typedUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return { getMe: result.login.user };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            typedUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return { getMe: result.register.user };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
 function MyApp({ Component, pageProps }: any) {
   return (
